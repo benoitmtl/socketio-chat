bd19fff0f7e143c7329ff81e7d8be979769630d0
bencactus <bencactus@users.noreply.github.com>
2017-07-13 07:36:37 -0400

    chat functions

--- server.js
+++ server.js
@@ -12,11 +12,14 @@ app.get('/', function(req,res){
 	res.sendFile( __dirname + '/index.html')
 });
 
-io.sockets.on('connection', socket => {
+io.sockets.on('connection', function(socket) {
 	connections.push(socket);
 	console.log('Connected: %s sockets connected', connections.length);
 
 	//disconnect
-	connections.splice(connections.indexOf(socket),1);
-	console.log('Disconnected: %s sockets connected', connections.length);
+	socket.on('disconnect', function(data){
+		connections.splice(connections.indexOf(socket),1);
+	    console.log('Disconnected: %s sockets connected', connections.length);
+	});
+	
 });
